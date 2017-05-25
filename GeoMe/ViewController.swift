72ec5603e407c11c3edcda2eacddc6a7cd0a85ac
Martin Kim Dung-Pham <kim@elbedev.com>
2017-05-25 22:15:07 +0200

    Initial Commit

---
+++ GeoMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GeoMe
+//
+//  Created by Martin Kim Dung-Pham on 25.05.17.
+//  Copyright © 2017 elbedev.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
