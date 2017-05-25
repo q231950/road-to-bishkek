068309ffc2c8122dacbd94b9719517dbd38eb0c1
Martin Kim Dung-Pham <kim@elbedev.com>
2017-05-25 23:14:57 +0200

    Display distance to Bishkek

--- GeoMe/ViewController.swift
+++ GeoMe/ViewController.swift
@@ -7,19 +7,64 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    
+    private let manager = CLLocationManager()
+    @IBOutlet weak var distanceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        manager.delegate = self
+        manager.desiredAccuracy = 10
+        
+        handleCurrentLocationAuthorizationStatus(CLLocationManager.authorizationStatus())
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func handleCurrentLocationAuthorizationStatus(_ status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+        default:
+            print("status is \(status)")
+        }
+        
+        if CLLocationManager.locationServicesEnabled() {
+            manager.startUpdatingLocation()
+        }
     }
+    
+    fileprivate func distanceToBishkek(location: CLLocation) -> CLLocationDistance {
+        let bishkek = CLLocation(latitude: CLLocationDegrees(42.874722), longitude: CLLocationDegrees(74.612222))
+        return location.distance(from: bishkek)
+    }
+}
 
-
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        print("locationManager authorization status is now")
+        switch status {
+        case .authorizedAlways:
+            print("authorizedAlways")
+        case .authorizedWhenInUse:
+            print("authorizedWhenInUse")
+        case .denied:
+            print("denied")
+        case .notDetermined:
+            print("notDetermined")
+        case .restricted:
+            print("restricted")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            let distance = distanceToBishkek(location: location)
+            distanceLabel.text = "\(distance/1000) km"
+            print(distance/1000)
+        }
+    }
 }
 
