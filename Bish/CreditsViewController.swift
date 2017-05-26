41c1c821631a454658ec1b62784f1dad10af84b9
Martin Kim Dung-Pham <kim@elbedev.com>
2017-05-26 23:20:17 +0200

    Add CreditsViewController with default status bar style

---
+++ Bish/CreditsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CreditsViewController.swift
+//  Bish
+//
+//  Created by Martin Kim Dung-Pham on 26.05.17.
+//  Copyright © 2017 elbedev.com. All rights reserved.
+//
+
+import UIKit
+
+class CreditsViewController: UIViewController {
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .default
+    }
+    
+}
