a58b8bc7dac3ac6a7941b960710505edc138c922
Martin Kim Dung-Pham <kim@elbedev.com>
2017-05-27 17:42:52 +0200

    Reformatting text

--- Bish/DistanceViewController.swift
+++ Bish/DistanceViewController.swift
@@ -11,6 +11,11 @@ import CoreLocation
 
 class DistanceViewController: UIViewController {
     
+    private let kBigTextFontSize: CGFloat = 60
+    private let kBigTextFontName: String = "Menlo-Bold"
+    private let kSmallTextFontSize: CGFloat = 32
+    private let kSmallTextFontName: String = "Menlo"
+    
     private let manager = CLLocationManager()
     @IBOutlet weak var distanceLabel: UILabel!
     
@@ -18,7 +23,7 @@ class DistanceViewController: UIViewController {
         super.viewDidLoad()
         
         manager.delegate = self
-        manager.desiredAccuracy = 10
+        manager.desiredAccuracy = 1
         
         handleCurrentLocationAuthorizationStatus(CLLocationManager.authorizationStatus())
     }
@@ -41,9 +46,81 @@ class DistanceViewController: UIViewController {
     }
     
     fileprivate func distanceToBishkek(location: CLLocation) -> CLLocationDistance {
-        let bishkek = CLLocation(latitude: CLLocationDegrees(42.874722),
-                                 longitude: CLLocationDegrees(74.612222))
+        
+        let bishkekCoordinates = CLLocationCoordinate2D(latitude: CLLocationDegrees(42.874722),
+                                                        longitude: CLLocationDegrees(74.612222))
+        let bishkek = CLLocation(coordinate: bishkekCoordinates, altitude: 800, horizontalAccuracy: 1, verticalAccuracy: 1, timestamp: Date())
+        
+        //print("\(location.altitude) - \(bishkek.altitude)")
         return location.distance(from: bishkek)
+    }
+    
+    fileprivate func updateWith(_ distance: CLLocationDistance) {
+        
+        let combinedString = NSMutableAttributedString()
+        
+        let kilometers = attributedKilometersStringWith(distance)
+        combinedString.append(kilometers)
+        
+        let meters = attributedMetersStringWith(distance)
+        combinedString.append(meters)
+        
+        let centimeters = attributedCentimetersStringWith(distance)
+        combinedString.append(centimeters)
+        
+        let city = attributedCityString(name: "Bishkek")
+        combinedString.append(city)
+
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .left
+
+        let attributes = [NSParagraphStyleAttributeName: paragraphStyle]
+        combinedString.addAttributes(attributes, range: NSMakeRange(0, combinedString.length))
+
+        distanceLabel.attributedText = combinedString
+    }
+    
+    private func attributedKilometersStringWith(_ distance: CLLocationDistance) -> NSAttributedString {
+        return attributedUnitString(unit: "kilometers", value: Int(distance.divided(by: 1000).rounded(.up)))
+    }
+    
+    private func attributedMetersStringWith(_ distance: CLLocationDistance) -> NSAttributedString {
+        return attributedUnitString(unit: "meters", value: Int(distance.truncatingRemainder(dividingBy: 1000)))
+    }
+    
+    private func attributedCentimetersStringWith(_ distance: CLLocationDistance) -> NSAttributedString {
+        return attributedUnitString(unit: "centimeters", value: Int(distance.multiplied(by: 100).truncatingRemainder(dividingBy: 100)))
+    }
+    
+    private func bigTextAttributes() -> [String: Any] {
+        return  [NSFontAttributeName: UIFont.init(name: kBigTextFontName, size: kBigTextFontSize)!,
+                 NSForegroundColorAttributeName: UIColor.white]
+    }
+    
+    private func smallTextAttributes() -> [String: Any] {
+        return [NSFontAttributeName: UIFont.init(name: kSmallTextFontName, size: kSmallTextFontSize)!,
+                NSForegroundColorAttributeName: UIColor.white]
+    }
+
+    private func attributedUnitString(unit: String, value: Int) -> NSAttributedString {
+        
+        let valueString = NSAttributedString(string: "\(value)", attributes: bigTextAttributes())
+        let unitString = NSAttributedString(string: "\(unit)", attributes: smallTextAttributes())
+        
+        let combinedString = NSMutableAttributedString()
+        combinedString.append(valueString)
+        combinedString.append(unitString)
+        return combinedString
+    }
+    
+    private func attributedCityString(name: String) -> NSAttributedString {
+        let valueString = NSAttributedString(string: "to", attributes: smallTextAttributes())
+        let unitString = NSAttributedString(string: "\(name)", attributes: bigTextAttributes())
+        
+        let combinedString = NSMutableAttributedString()
+        combinedString.append(valueString)
+        combinedString.append(unitString)
+        return combinedString
     }
 }
 
@@ -67,12 +144,8 @@ extension DistanceViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.first {
             let distance = distanceToBishkek(location: location)
-            let nf = NumberFormatter()
-            nf.numberStyle = .decimal
-            let attributes = [NSFontAttributeName: UIFont.init(name: "Menlo-Bold", size: 19)!,
-                              NSForegroundColorAttributeName: UIColor.white]
-            let paragraph = NSAttributedString(string: "\(distance/1000) km to Bishkek", attributes: attributes)
-            distanceLabel.attributedText = paragraph
+            updateWith(distance)
+            print(distance)
         }
     }
 }
---
+++ Bish/DistanceViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  DistanceViewModel.swift
+//  Bish
+//
+//  Created by Martin Kim Dung-Pham on 27.05.17.
+//  Copyright © 2017 elbedev.com. All rights reserved.
+//
+
+import UIKit
+
+struct DistanceViewModel {
+    
+}
